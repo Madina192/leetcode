99d8227bfcc41f94b9e8d2268208138f320eed71
Madina <madinaakpaeva000@gmail.com>
2023-10-08 20:29:46 +0600

    solved FirstUniqueLetter :)

--- src/FirstUniqueLetter.java
+++ src/FirstUniqueLetter.java
@@ -5,42 +5,45 @@ import java.util.Scanner;
 public class FirstUniqueLetter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        String s = scanner.next();
         char[] charArray = s.toCharArray();
-        int indexOfUniqueLetter = -1;
-        ArrayList listOfRepeatedEl = new ArrayList<>();
-        boolean isItOccurs = false;
+        int[] freq = new int[26];
+        int indexOfUnique = -1;
+        char firstUniqueLetter = ' ';
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         for (int i = 0; i < charArray.length; i++) {
-            if (i == charArray.length - 1) {
-                if (listOfRepeatedEl.contains(charArray[i])) {
-                    isItOccurs = true;
-                } else {
-                    isItOccurs = false;
+            for (int j = 0; j < alphabet.length; j++) {
+                if (charArray[i] == alphabet[j]) {
+                    freq[j]++;
                 }
-            } else {
-                for (int j = i + 1; j < charArray.length; j++) {
-                    if (charArray[i] == charArray[j]) {
-                        isItOccurs = true;
-                        listOfRepeatedEl.add(charArray[i]);
-                        System.out.println("occurs " + charArray[i]);
-                    } else if (listOfRepeatedEl.contains(charArray[i])) {
-                        isItOccurs = true;
-                    } else {
-                        isItOccurs = false;
-
-                    }
-                    System.out.println("letters: " + charArray[i] + charArray[j]);
-                }
-            }
-            if (isItOccurs) {
-                indexOfUniqueLetter = -1;
-                System.out.println("true");
-            } else {
-                indexOfUniqueLetter = i;
-                break;
             }
         }
-        System.out.println(indexOfUniqueLetter);
-        System.out.println(Arrays.toString(charArray));
+        boolean isFound = false;
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = 0; j < freq.length; j++) {
+                if (!isFound) {
+                    if (charArray[i] == alphabet[j] && freq[j] == 1) {
+                        //System.out.println("element: " + charArray[i]);
+                        firstUniqueLetter = charArray[i];
+                        indexOfUnique = i;
+                        isFound = true;
+                    }
+                }
+            }
+        }
+//        for (int i = 0; i < freq.length; i++) {
+//            if (freq[i] == firstUniqueLetter) {
+//                indexOfUnique = i;
+//            }
+//        }
+        System.out.println(indexOfUnique);
+        System.out.println(firstUniqueLetter);
+        System.out.println(Arrays.toString(freq));
+        System.out.println(Arrays.toString(alphabet));
     }
 }
+//from leetcode:
+// int freq[] = new int[26];
+//    for(char i: s.toCharArray()) freq[i-'a']++;
+//    for(int i = 0; i < s.length(); i++) if(freq[s.charAt(i)-'a'] == 1) return i;
+//    return -1;
